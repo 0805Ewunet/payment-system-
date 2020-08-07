dd1c8773a5de65c1b51337039eb7f5e245a7734c
Ewunet Kassa <ewunetkassa@gmail.com>
2020-08-06 20:00:07 -0400

    Updates

---
+++ precache-manifest.b59dacde43d30c71714f425a10c3d74d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd3cadcac4a73e7d69f533d733dbd5ab",
+    "url": "/0805Ewunetindex.html"
+  },
+  {
+    "revision": "35f9e022eddeb16e7c5b",
+    "url": "/0805Ewunetstatic/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ce11b16daf0fcd8b629a",
+    "url": "/0805Ewunetstatic/js/2.31194cdd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/0805Ewunetstatic/js/2.31194cdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35f9e022eddeb16e7c5b",
+    "url": "/0805Ewunetstatic/js/main.2cd94ac8.chunk.js"
+  },
+  {
+    "revision": "c762a2d87a816b1c6e1c",
+    "url": "/0805Ewunetstatic/js/runtime-main.3f5599da.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/0805Ewunetstatic/media/logo.5d5d9eef.svg"
+  }
+]);
